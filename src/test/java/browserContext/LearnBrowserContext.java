f0a30dfbe0c15cf1d0b9803a65b01d1d5c1941f9
Akmal Rizaev <testing300bucks@gmail.com>
2024-06-27 16:35:06 +0500

    browser context 2

--- src/test/java/browserContext/LearnBrowserContext.java
+++ src/test/java/browserContext/LearnBrowserContext.java
@@ -1,6 +1,7 @@
 package browserContext;
 
 import com.microsoft.playwright.*;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import com.microsoft.playwright.options.AriaRole;
 
 public class LearnBrowserContext {
@@ -14,7 +15,23 @@ public class LearnBrowserContext {
         page.getByLabel("Password").type("qweqwe");
         page.getByRole(AriaRole.BUTTON,
                 new Page.GetByRoleOptions().setName("Login")).click();
+                Locator myAccount = page.getByText("Edit your account information");
+        assertThat(myAccount).isVisible();
 
+        Page newTab = page.context().newPage();
+        newTab.navigate("https://ecommerce-playground.lambdatest.io/index.php?route=account/account");
+        assertThat(myAccount).isVisible();
+        newTab.close();
+        context.close();
+
+        BrowserContext context2 = browser.newContext();
+        Page userPage = context2.newPage();
+        userPage.navigate("https://ecommerce-playground.lambdatest.io/index.php?route=account/account");
+
+        BrowserType firefox = playwright.firefox();
+        firefox.launch(new BrowserType.LaunchOptions().setHeadless(false)).newPage();
+
+        playwright.close();
 
     }
 }
